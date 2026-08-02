d8f199ac62364889adf79b7485d722107f607ed3
mkhaque1 <mkhaque29@gmail.com>
2026-08-02 14:30:21 +0200

    data type practise

---
+++ app.py
@@ -0,0 +1,24 @@
+# data type of python
+
+#1. Integer: Whole numbers without a decimal point. Example: 1, 2, 3, -4
+x = 5
+
+#2 . Float: Numbers with a decimal point. Example: 1.0, 2.5, -3.14
+
+y = 3.14
+
+z = x - y
+
+print(z)
+#3. string type . example : text/string use '', ""
+
+firstName = 'MK'
+lastName = 'Haque'
+
+print(firstName + ' ' + lastName)
+
+
+
+
+
+
