c485d49a48fcdbd16957cf3d64375b232d6283cd
mkhaque1 <mkhaque29@gmail.com>
2026-08-02 14:58:09 +0200

    refactor file name and add str methods

---
+++ string-methods.py
@@ -0,0 +1,28 @@
+# str methods
+
+#capitalize()
+
+name = 'mkhaque'
+
+capitalize_name = name.capitalize()
+
+print(capitalize_name)
+
+# upper() / lower()
+
+upper_name = name.upper()
+lower_name = name.lower()
+print(upper_name)
+
+# title()
+
+title_name = name.title()
+print(title_name)
+
+#encode() / decode()
+
+encode = name.encode('utf-8')
+print(encode)
+
+decode = encode.decode('utf-8')
+print(decode)
---
+++ types.py
@@ -0,0 +1,43 @@
+# data type of python
+
+#1. Integer: Whole numbers without a decimal point. Example: 1, 2, 3, -4
+x = 5
+
+#2 . Float: Numbers with a decimal point. Example: 1.0, 2.5, -3.14
+
+y = 3.14
+
+z = x - y
+
+print(z)
+#3. string type . example : text/string use '', ""
+
+firstName = 'MK'
+lastName = 'Haque'
+
+print(firstName + ' ' + lastName)
+
+# 4. Boolean: Represents True or False values. Example: True, False
+
+is_student = True
+is_teacher = False
+
+print(type(is_student))
+print(type(is_teacher))
+
+#5. List: An ordered collection of items. Example: [1, 2, 3], ['apple', 'banana', 'cherry']
+
+car = ['Toyota', 'Honda', 'Ford']
+
+number = [1, 2, 3, 4, 5]
+
+print(type(car))
+print(type(number))
+
+#6. dictionary: A collection of key-value pairs. Example: {'name': 'John', 'age': 30}
+
+person = {'name': 'John', 'age': 30}
+
+mkhaque = {'name': 'MK', 'age' : 30, 'city' : 'Essen'}
+
+print(type(mkhaque))
