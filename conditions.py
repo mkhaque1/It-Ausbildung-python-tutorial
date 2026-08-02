b096bc25833023b1dfe419dbdbbcd93a6a7f7959
mkhaque1 <mkhaque29@gmail.com>
2026-08-02 15:16:24 +0200

    add conditions.py with basic if-else statements and comments

---
+++ conditions.py
@@ -0,0 +1,25 @@
+# conditions in python , if,  else,  elif
+# == equal to
+# != not equal to
+# >= greater than or equal to
+# <= less than or equal to
+
+name = 'khairul'
+age = 9
+
+if name == 'MK' and age == 30:
+    print('Name is MK and age is 30')
+elif name == 'MK' and age != 30:
+    print('Name is MK but age is not 30')
+else:
+    print('Name is not MK')
+
+
+if age >= 18:
+    print(name + ' is an adult')
+else:
+    print(name + ' is not an adult')
+
+# student result: pass or fail ?
+
+
